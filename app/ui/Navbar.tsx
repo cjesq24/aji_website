a5493f3dff87ec419a939d8823f5dc1b4e6fa91e
Chad Jackson <39926212+cjesq24@users.noreply.github.com>
2024-03-12 16:37:42 -0400

    Navbar fix

--- app/ui/Navbar.tsx
+++ app/ui/Navbar.tsx
@@ -1,9 +1,8 @@
-'use client'
-import {Fragment} from 'react';
+'use client';
+import { Fragment } from 'react';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline';
 import Image from 'next/image';
-
 
 const navigation = [
 	{ name: 'About', href: '/about', current: true },
@@ -79,11 +78,7 @@ export default function NavBar() {
 										<Menu.Button className='relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800'>
 											<span className='absolute -inset-1.5' />
 											<span className='sr-only'>Open user menu</span>
-											<Image
-												className='h-8 w-8 rounded-full'
-												src=''
-												alt=''
-											/>
+											<Image className='h-8 w-8 rounded-full' src='' alt='' />
 										</Menu.Button>
 									</div>
 									<Transition
@@ -94,8 +89,22 @@ export default function NavBar() {
 										leave='transition ease-in duration-75'
 										leaveFrom='transform opacity-100 scale-100'
 										leaveTo='transform opacity-0 scale-95'>
-										<Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none'>
-											<Menu.Item>
+										<Menu.Items className='absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-white ring-opacity-5 focus:outline-none'>
+											{navigation.map(n => (
+												<Menu.Item key={n.name}>
+													<a
+														href={n.href}
+														key={n.name}
+														className={classNames(
+															n.current ? 'bg-gray-100' : '',
+															'block px-4 py-2 text-sm text-gray-700'
+														)}>
+														{n.name}
+													</a>
+												</Menu.Item>
+											))}
+										</Menu.Items>
+										{/* <Menu.Item>
 												{({ active }) => (
 													<a
 														href='#'
@@ -131,14 +140,14 @@ export default function NavBar() {
 													</a>
 												)}
 											</Menu.Item>
-										</Menu.Items>
+										</Menu.Items> */}
 									</Transition>
 								</Menu>
 							</div>
 						</div>
 					</div>
 
-					<Disclosure.Panel className='sm:hidden'>
+					<Disclosure.Panel className='md:hidden'>
 						<div className='space-y-1 px-2 pb-3 pt-2'>
 							{navigation.map(item => (
 								<Disclosure.Button
